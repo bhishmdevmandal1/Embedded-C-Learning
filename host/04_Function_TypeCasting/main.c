495e0899021042dcb0e426a919f816737ffbb54e
Bhishmdev Mandal <bhishmdev123321@gmail.com>
2026-09-23 23:19:58 +0530

    Add data truncation and explicit float casting examples with logic notes

--- host/04_Function_TypeCasting/main.c
+++ host/04_Function_TypeCasting/main.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 int main()
@@ -7,8 +6,6 @@ int main()
 
 0x87 + 0xFF00 -> compiler implicitly considers the size of these constants as 4 bytes (of integer data type ).This is also called as integer promotion.
 So, compiler did implicit casting twice here 1.It promoted the date size of RHS constant to 'int' 2.While storing the RHS into LHS it again did casting from 'int' to 'unsigned char' and fired the warning "data loss during the conversion". */
-
-
 
 
 	float result = (float)80 /3;		//(float)80 -> (explicit) Promoted to float, 3 ->(implicit) promoted to float . So, the division is now float divided float which yields float.
