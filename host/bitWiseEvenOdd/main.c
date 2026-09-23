55a49eb9ed7202c8d7323a4f1e69749574ec01d7
Bhishmdev Mandal <bhishmdev123321@gmail.com>
2026-09-24 01:38:18 +0530

    Add even odd checker using bitwise AND operator

---
+++ host/bitWiseEvenOdd/main.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdint.h>
+
+void wait_for_user_input(void);
+
+int main(void)
+{
+	int32_t num1;
+	printf("Enter a Number :");
+	fflush(stdout);
+	scanf("%d",&num1);
+
+	if(num1 & 1){
+		printf("%d is odd number\n",num1);
+	}else{
+		printf("%d is even number\n",num1);
+	}
+
+	wait_for_user_input();
+
+}
+
+void wait_for_user_input(void)
+	{
+		printf("Press enter key to exit this application\n");
+		fflush(stdout);
+		while(getchar() != '\n')
+		{
+		  //just read the input buffer & do nothing(for command prompt)
+		}
+		getchar();
+	}
